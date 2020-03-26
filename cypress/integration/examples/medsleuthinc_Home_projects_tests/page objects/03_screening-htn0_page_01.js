76b00535e3e5d28406c774f5b8e195fdb64f2903
moocallteen <kukarinworkspace@gmail.com>
2020-03-26 19:23:48 +0200

    UPD

---
+++ cypress/integration/examples/medsleuthinc_Home_projects_tests/page objects/03_screening-htn0_page_01.js
@@ -0,0 +1,13 @@
+module.exports ={
+    introStartSurBtn : 'button[aria-describedby="dialog_prolog"]',
+    introText : 'p[class="dialog_prolog__node"]',
+    // text = Do you take any medications to control your Blood Pressure?
+    yesRadioBtn : 'input[id="screening.htn.0.item.yes"]',
+    noRadioBtn : 'input[id="screening.htn.0.item.no"]',
+    nextBtn : 'button[aria-describedby="dialog_prolog entry_prolog"]',
+    medicationQtySelect : 'select[name="donor_screen_rx_bp_3_query_entry_0"]', // 'select[id="selectID"]', //'span[class="grommetux-form-field__contents"]', 
+    //values: 1, 2, >2
+    //text = How many medications do you take to control your blood pressure?
+    validationErrorMsg : 'span[class="grommetux-form-field__error"]',
+
+}
---
+++ cypress/integration/examples/medsleuthinc_Home_projects_tests/tests/test-03_screening-htn0_page_01.js
@@ -0,0 +1,47 @@
+import screeningHtn0 from "../page objects/03_screening-htn0_page_01";
+
+describe("transplan kidney donor page test", () => {
+    before(() => {
+      Cypress.config(
+        "baseUrl",
+        "https://mobile-survey.patientfollowup.org.arcgmrqa.i.mymedsleuth.com/transplant-kidney/donor-prereq-1/dialogs"
+      );
+      cy.viewport(1280, 800);
+    });
+
+    it("Should check the screening.htn.0 page elements", () => {
+        var selectorText = "How many medications do you take to control your blood pressure?";
+        var medicationQtyTxt = "How many medications do you take to control your blood pressure?";
+      cy.visit("/screening.htn.0")
+        .get(screeningHtn0.introText)
+        .should("be.visible")
+        .get(screeningHtn0.introStartSurBtn)
+        .should("be.visible")
+        .click()
+        .get(screeningHtn0.nextBtn)
+        .should("be.visible")
+        .and("contain", "Next")
+        .click()
+        .get(screeningHtn0.validationErrorMsg)
+        .should("be.visible")
+        .and("contain", "Required")
+        .get(screeningHtn0.noRadioBtn)
+        .should("be.visible")
+        .get(screeningHtn0.yesRadioBtn)
+        .should("be.visible")
+        .check()
+        .get(screeningHtn0.nextBtn).click()
+        .get(screeningHtn0.medicationQtyTxt).should("be.visible")
+        .get(screeningHtn0.medicationQtySelect)//.should("be.visible")
+        .then(el => {
+            assert.include(el.text(), "Select One")
+        })
+
+        
+
+        // .then(el => {
+        //   assert.include(el.text(), );
+    });
+
+
+})
