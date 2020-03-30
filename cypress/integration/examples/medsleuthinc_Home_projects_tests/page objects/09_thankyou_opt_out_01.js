49ac47340552e31216f34871b54d0b3b2b6ed9c5
moocallteen <kukarinworkspace@gmail.com>
2020-03-30 18:53:28 +0300

    UPD 18:53

---
+++ cypress/integration/examples/medsleuthinc_Home_projects_tests/page objects/09_thankyou_opt_out_01.js
@@ -0,0 +1,7 @@
+module.exports ={
+    introStartSurBtn : 'button[aria-describedby="dialog_prolog"]',
+    nextBtn : 'button[id="next-button"]',
+    headerBar : 'div[class="dialog_title"]',
+    thankYouContent : 'div[class="qAndBtns"]',
+    contentContainer : 'label[id="radioLabel"]'
+}
