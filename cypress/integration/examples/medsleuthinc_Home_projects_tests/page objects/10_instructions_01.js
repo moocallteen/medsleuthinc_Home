180e3cc1d5adc1e60cc7cc85bad1ae413f014138
moocallteen <kukarinworkspace@gmail.com>
2020-03-31 13:46:41 +0300

    UPD 13:46

---
+++ cypress/integration/examples/medsleuthinc_Home_projects_tests/page objects/10_instructions_01.js
@@ -0,0 +1,9 @@
+module.exports ={
+    introStartSurBtn : 'button[aria-describedby="dialog_prolog"]',
+    nextBtn : 'button[id="next-button"]',
+    validationErrorMsg : 'span[class="grommetux-form-field__error"]',
+    enterMedicationsRadioBtn : 'label[for="item.instructions.04.1"]',
+    doNotTakeMedsRadioBtn : 'label[for="item.instructions.04.2"]',
+    headerBar : 'div[class="dialog_title"]'
+
+}
