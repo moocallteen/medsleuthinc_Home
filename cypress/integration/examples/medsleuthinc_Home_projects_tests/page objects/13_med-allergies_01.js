8719f92e6559cdaa11eeec295ec1752030fd699d
moocallteen <kukarinworkspace@gmail.com>
2020-04-07 13:57:18 +0300

    UPD 13:57

---
+++ cypress/integration/examples/medsleuthinc_Home_projects_tests/page objects/13_med-allergies_01.js
@@ -0,0 +1,26 @@
+module.exports = {
+    introStartSurBtn : 'button[aria-describedby="dialog_prolog"]',
+    validationErrorMsg : 'span[class="grommetux-form-field__error"]',
+    nextBtn : 'button[id="next-button"]',
+    headerBar : 'div[class="dialog_title"]',
+
+    yesMedAllergies : 'label[for="choice-meds-allergies-yes"]',
+    noMedAllergies : 'label[for="choice-meds-allergies-no"]',
+
+    medsInputField : 'input[id="autocomplete_input"]',
+    placeHolder : 'input[placeholder="Enter name of medication"]',
+    medsListWrapper : 'ul[class="react-autosuggest__suggestions-list"]',
+    firstSuggestedItem : 'li[id="react-autowhatever-autocomplete_input--item-1"]',
+
+    makeChangesBtn : 'a[class="btn-secondary btn-block makeChangesBtn mt-4 priCol-border btnFontSize"]',
+    deleteMedBtn : 'button[class="grommetux-button grommetux-button--plain"]',
+
+    anaphylaxisReactionCheckBox : 'label[for="choice.ANAPHYLAXIS.3776"]',
+    anxietyReactionCheckBox : 'label[for="choice.anxiety.3776"]',
+    breathIssuesReactionCheckBox : 'label[for="choice.respiratory.3776"]',
+    nauseaReactionCheckBox : 'label[for="choice.nausea.3776"]',
+    rashHivesReactionCheckBox : 'label[for="choice.rash.3776"]',
+    swellingReactionCheckBox : 'label[for="choice.swelling.3776"]',
+    otherReactionCheckBox : 'label[for="choice.other.3776"]',
+    otherInputField : 'input[id="choice.3776.0"]'
+}
