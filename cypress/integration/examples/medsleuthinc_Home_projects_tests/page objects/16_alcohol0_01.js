c1cadc644507d2f7b71182f6b68f604a039f21fc
moocallteen <kukarinworkspace@gmail.com>
2020-04-09 12:05:26 +0300

    UPD 12:05

---
+++ cypress/integration/examples/medsleuthinc_Home_projects_tests/page objects/16_alcohol0_01.js
@@ -0,0 +1,16 @@
+module.exports = {
+    introStartSurBtn : 'button[aria-describedby="dialog_prolog"]',
+    validationErrorMsg : 'span[class="grommetux-form-field__error"]',
+    nextBtn : 'button[id="next-button"]',
+    headerBar : 'div[class="dialog_title"]',
+
+    neverRadioBtn : 'label[for="query.alcohol.entry.1.item.0"]',
+    yearOnceRadioBtn : 'label[for="query.alcohol.entry.1.item.1"]',
+    monthOnceRadioBtn : 'label[for="query.alcohol.entry.1.item.2"]',
+    weekOnceRadioBtn : 'label[for="query.alcohol.entry.1.item.3"]',
+    everyDayTwoRadioBtn : 'label[for="query.alcohol.entry.1.item.4"]',
+    everyDayMoreRadioBtn : 'label[for="query.alcohol.entry.1.item.5"]',
+
+    yesAbuseRadioBtn : 'label[for="alcohol.query.0.entry.0.item.yes"]',
+    noAbuseRadioBtn : 'label[for="alcohol.query.0.entry.0.item.no"]',
+}
