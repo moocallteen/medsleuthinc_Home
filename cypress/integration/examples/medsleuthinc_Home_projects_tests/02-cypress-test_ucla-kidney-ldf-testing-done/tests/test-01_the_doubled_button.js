f86e6faf57169d6f0a4d0c1a6ae5a8a897d4de03
moocallteen <kukarinworkspace@gmail.com>
2020-05-08 15:43:46 +0300

    UPD 15:43

---
+++ cypress/integration/examples/medsleuthinc_Home_projects_tests/02-cypress-test_ucla-kidney-ldf-testing-done/tests/test-01_the_doubled_button.js
@@ -0,0 +1,38 @@
+import doubledBtnUCLAldf from "../page-object/01_the_doubled_button";
+
+describe("transplant kidney donor page test", () => {
+  before(() => {
+    Cypress.config(
+      "baseUrl",
+      "https://mobile-survey.mymedsleuth.com/ucla-transplant-kidney-followup/donor-followup-0/dialogs/"
+    );
+    cy.viewport(1280, 800);
+  });
+
+  it("Should check the supplemental.ldf dialog's [the doubled button] issue's fix", () => {
+
+    // link to the ticket [https://trello.com/c/0JyxE7xw]
+
+    /*
+    1. navigate to the "supplemental.ldf" dialog
+    2. check the title of the dialog
+    3. click to start survey
+    4. check the visibility of the button
+    */
+
+    var supplementalHeading = "Is there any additional information that you would like shared with your health care providers? (optional)";
+
+    cy.visit("/supplemental.ldf")    //1. navigate to the "supplemental.ldf" dialog
+      .contains(supplementalHeading)   //2. check the title of the dialog
+      .get(doubledBtnUCLAldf.introStartSurBtn)
+      .should("be.visible")
+      .and("have.text", "Start Survey")
+      .click();    //3. click to start survey
+
+    cy.get(doubledBtnUCLAldf.submitBtn)   //4. check the visibility of the button
+      .should("be.visible")
+      .and("have.text", "Submit");
+
+  });
+
+})
