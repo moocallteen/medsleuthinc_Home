b24c55a4162c9601b2a9c7c94b3c68596a39630d
moocallteen <kukarinworkspace@gmail.com>
2020-05-08 17:53:49 +0300

    UPD 17:53

---
+++ cypress/integration/examples/medsleuthinc_Home_projects_tests/04-cypress-test_utah-kidney-ldf-testing-done/page-objects/02_title_of_button_is_shifted.js
@@ -0,0 +1,5 @@
+module.exports = {
+  introStartSurBtn: 'button[aria-describedby="dialog_prolog"]',
+
+  submitBtn: 'button[class="grommetux-button grommetux-button--primary grommetux-button--plain"]'
+}
